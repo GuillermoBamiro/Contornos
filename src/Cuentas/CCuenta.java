c1ed203817456ee6fea92f690dd3740f77b59dca
GuillermoBamiro <guillermodominguezof@gmail.com>
2022-05-23 21:50:06 +0200

    comit comentarios

--- src/Cuentas/CCuenta.java
+++ src/Cuentas/CCuenta.java
@@ -1,92 +1,128 @@
 package Cuentas;
-
+/**
+* Operativo de Cuentas
+* Esta clase se encarga del operativo de cuentas
+* @author Guillermo Dominguez Suarez
+* @version 0.1, 21/05/2022
+*/
 public class CCuenta {
-
     /**
-     * @return the nombre
+     * @param nombre Nombre titular cuenta
+     */
+    private String nombre;
+    /**
+     *@param cuenta numero de la cuenta bancaria 
+     */
+    private String cuenta;
+    /**
+     * @param saldo saldo de la cuenta
+     */
+    private double saldo;
+    /**
+     * @param tipo de interes Tipo de interes de la cuenta
+     */
+    private double tipoInterés;
+    
+    /**
+     * @return nombre
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * @param nombre
+     * metodo set nombre
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * @return cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * @param cuenta 
+     * metodo set de cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * @return saldo
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * @param saldo 
+     * metodo set de saldo
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * @return tipoInterés
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * @param tipoInterés
+     * metodo set de tipoInterés
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
 
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
-
-    public CCuenta()
-    {
-    }
-
+/**
+ *  Metodo iniciador de los parametro de clase CCuenta
+ * @param nom inicia nombre
+ * @param cue inicia cuenta
+ * @param sal inicia saldo
+ * @param tipo inicia tipoInteres
+ */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+/**
+ * 
+ * @return el saldo de la cuenta
+ */
     public double estado()
     {
         return getSaldo();
     }
-
+/**
+ * Metodo que lleva la operativa de ingresos en cuenta
+ * @param cantidad
+ * @throws Exception si el ingreso es inferior a cero
+ */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+/**
+ * metodo que lleva la operativa de retirada de cuenta y resta lo retirado al 
+ * saldo
+ * @param cantidad
+ * @throws Exception Si la cantidad a retirar es negativa
+ * si el saldo es menor a la cantidad
+ */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
--- src/Cuentas/Main.java
+++ src/Cuentas/Main.java
@@ -8,13 +8,15 @@ public class Main {
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es "+ saldoActual );
 
-        operativa_cuenta(cuenta1);
+        operativa_cuenta(cuenta1, 30.02F);
     }
 
-    private static void operativa_cuenta(CCuenta cuenta1) {
-        
+    private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+        float c=cantidad; /*Non podo refactorizar ningun dos valores existentes
+        a float xa que son de tipo int e ao refactorizar asigna xa o tipo de 
+        dato*/
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
